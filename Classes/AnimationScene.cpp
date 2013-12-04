73a57f638bc098598bb08124a99733ef9aab9160
jiz869 <freeprogrammer@gmail.com>
2013-12-04 16:34:09 -0500

    animation practice

---
+++ Classes/AnimationScene.cpp
@@ -0,0 +1,159 @@
+#include "AnimationScene.h"
+#include "GameOverScene.h"
+#include "SimpleAudioEngine.h"
+
+//using namespace cocos2d;
+
+
+//////////////////////////////////////////////////////////////
+
+float AnimationScene::makeBatFly( SimpleAnimObject *bat ) 
+{
+
+}
+
+void AnimationScene::makeBatGlideDown( SimpleAnimObject* bat )
+{
+
+}
+
+void AnimationScene::step( float dt )
+{
+
+}
+
+///////////////////////////////////////////////////////
+
+AnimationScene::~AnimationScene()
+{
+
+	// cpp don't need to call super dealloc
+	// virtual destructor will do this
+}
+
+AnimationScene::AnimationScene()
+{
+}
+
+CCScene* AnimationScene::scene()
+{
+	CCScene * scene = NULL;
+	do
+	{
+		// 'scene' is an autorelease object
+		scene = CCScene::create();
+		CC_BREAK_IF(! scene);
+
+		// 'layer' is an autorelease object
+		AnimationScene *layer = AnimationScene::create();
+		CC_BREAK_IF(! layer);
+
+		// add layer as a child to scene
+		scene->addChild(layer);
+	} while (0);
+
+	// return the scene
+	return scene;
+}
+
+static int getRandom(int low, int high)
+{
+	if ( low - high < 0x10000L )
+	      return low + ( ( random() >> 8 ) % ( high + 1 - low ) );
+
+	return low + ( random() % ( high + 1 - low ) );
+}
+
+// on "init" you need to initialize your instance
+bool AnimationScene::init()
+{
+	bool bRet = false;
+	do
+	{
+		//////////////////////////////////////////////////////////////////////////
+		// super init first
+		//////////////////////////////////////////////////////////////////////////
+
+		CC_BREAK_IF(! CCLayerColor::initWithColor( ccc4(255,255,255,255) ) );
+
+		//////////////////////////////////////////////////////////////////////////
+		// add your codes below...
+		//////////////////////////////////////////////////////////////////////////
+
+		// 1. Add a menu item with "X" image, which is clicked to quit the program.
+
+		// Create a "close" menu item with close icon, it's an auto release object.
+		CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
+			"CloseNormal.png",
+			"CloseSelected.png",
+			this,
+			menu_selector(AnimationScene::menuCloseCallback));
+		CC_BREAK_IF(! pCloseItem);
+
+		// Place the menu item bottom-right conner.
+        CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
+        CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
+
+		pCloseItem->setPosition(ccp(origin.x + visibleSize.width - pCloseItem->getContentSize().width/2,
+                                    origin.y + pCloseItem->getContentSize().height/2));
+
+		// Create a menu with the "close" menu item, it's an auto release object.
+		CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
+		pMenu->setPosition(CCPointZero);
+		CC_BREAK_IF(! pMenu);
+
+		// Add the menu to AnimationScene layer as a child layer.
+		this->addChild(pMenu, 1);
+
+		///////////////////////////////////////////////
+		// 2. add your codes below...
+        bats = CCArray::createWithCapacity( 10 );
+
+
+
+        ///////////////////////////////////////////////
+		this->setTouchEnabled(true);
+
+		// use updateGame instead of update, otherwise it will conflit with SelectorProtocol::update
+		// see http://www.cocos2d-x.org/boards/6/topics/1478
+		//this->schedule( schedule_selector(AnimationScene::updateGame) );
+
+		//CocosDenshion::SimpleAudioEngine::sharedEngine()->playBackgroundMusic("background-music-aac.wav", true);
+
+		bRet = true;
+	} while (0);
+
+	return bRet;
+}
+
+void AnimationScene::menuCloseCallback(CCObject* pSender)
+{
+	// "close" menu item clicked
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
+    CCMessageBox("You pressed the close button. Windows Store Apps do not implement a close button.", "Alert");
+#else
+    CCDirector::sharedDirector()->end();
+#endif
+}
+
+// cpp with cocos2d-x
+void AnimationScene::ccTouchesEnded(CCSet* touches, CCEvent* event)
+{
+	// Choose one of the touches to work with
+	CCTouch* touch = (CCTouch*)( touches->anyObject() );
+	CCPoint location = touch->getLocation();
+
+	CCLog("++++++++after  x:%f, y:%f", location.x, location.y);
+
+	// Set up initial location of projectile
+	CCSize winSize = CCDirector::sharedDirector()->getVisibleSize();
+    CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
+
+}
+
+void AnimationScene::registerWithTouchDispatcher()
+{
+	// CCTouchDispatcher::sharedDispatcher()->addTargetedDelegate(this,0,true);
+    CCDirector::sharedDirector()->getTouchDispatcher()->addStandardDelegate(this,0);
+}
+
---
+++ Classes/AnimationScene.h
@@ -0,0 +1,80 @@
+#ifndef __ANIMATION_SCENE_H__
+#define __ANIMATION_SCENE_H__ 
+
+#include "cocos2d.h"
+
+using namespace cocos2d;
+
+enum {
+	TAG_CASTLE = 0,
+	TAG_LIGHTNING_BOLT = 1,
+	TAG_BATS = 2
+};
+
+//Bat animation types
+enum {
+	BAT_FLYING_UP = 0,
+	BAT_GLIDING_DOWN = 1
+};
+
+////////// define of SimpleAnimObject //////////////////
+class SimpleAnimObject : public CCSprite {
+    public:
+        virtual ~SimpleAnimObject() {
+        };
+
+        // data 
+        int animationType;
+        CCPoint velocity;
+
+        virtual void update( float time ) {
+            this->m_obPosition = ccp( this->m_obPosition.x + velociy.x, this->m_obPosition.y + velociy.y );
+        }
+
+        CCRect rect() {
+            float scaleMod = 0.5f;
+            float w = this->getContentSize().width * this->getScale() * scaleMod;
+            float h = this->getContentSize().height * this->getScale() * scaleMod;
+            CCPoint point = CCPointMake( this->m_obPosition.x - w/2 , this->m_obPosition.y - h/2 );
+            return CCRectMake( point.x, point.y, w, h );
+        };
+};
+
+//////////////////////////////////////////////////////////////////
+class AnimationScene : public cocos2d::CCLayerColor
+{
+public:
+	AnimationScene();
+	~AnimationScene();
+
+	// Here's a difference. Method 'init' in cocos2d-x returns bool,
+    // instead of returning 'id' in cocos2d-iphone
+	virtual bool init();
+
+	// there's no 'id' in cpp, so we recommand to return the exactly class pointer
+	static cocos2d::CCScene* scene();
+
+	// a selector callback
+	virtual void menuCloseCallback(cocos2d::CCObject* pSender);
+
+	// implement the "static node()" method manually
+	CREATE_FUNC(AnimationScene);
+
+	void registerWithTouchDispatcher();
+	void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
+
+    float makeBatFly( SimpleAnimObject *bat ); 
+    void makeBatGlideDown( SimpleAnimObject *bat );
+    void step( float dt );
+
+protected:
+    CCArray *bats;
+    CCAnimation *batFlyUp;
+    CCAnimation *batGlideDown;
+    CCSprite *lightningBolt;
+    CCSprite *lightningGlow;
+    int lightningRemoveCount;
+};
+
+#endif  // __ANIMATION_SCENE_H__ 
+
