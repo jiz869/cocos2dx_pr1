33ec192d5d5eb6bfaff0b913b0d4164baf960a10
jiz869 <freeprogrammer@gmail.com>
2014-03-21 16:58:13 -0400

    tiles

--- Classes/Tiles.h
+++ Classes/Tiles.h
@@ -1,5 +1,18 @@
 #ifndef _TILES_H_
 #define _TILES_H_
 
+#include "cocos2d.h"
+using namespace cocos2d;
+
+class GTiles : public CCObject {
+    CCSpriteBatchNode *batchNode;
+
+public:
+    GTiles();
+    ~GTiles();
+    CCSpriteBatchNode* GetBatchNode();
+
+};
+
 #endif
 
