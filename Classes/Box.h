c904a192198e96110f49c8196f0a71fbdd69fe00
jiz869 <freeprogrammer@gmail.com>
2014-03-25 16:55:16 -0400

    map object v0.1

---
+++ Classes/Box.h
@@ -0,0 +1,24 @@
+#ifndef _BOX_H
+#define _BOX_H
+#include "GameObject.h"
+
+// box is a game object which can be regarded as a large box
+
+class GBox : public GObject {
+    CCSpriteBatchNode *batchNode;
+    CCTexture2D *texture;
+    CCPoint velocity;
+public:
+    virtual void Load();
+    virtual void SetPosition(float x, float y);
+    virtual CCNode* Node() {
+        return (CCNode*)batchNode;
+    }
+    virtual void Step(float dt) {
+    }
+
+    int width;
+    int height;
+};
+
+#endif;
---
+++ Classes/GameObject.h
@@ -0,0 +1,26 @@
+#ifndef _GAME_OBJECT_H_
+#define _GAME_OBJECT_H_
+
+#include "cocos2d.h"
+using namespaces cocos2d
+
+class GObject : public CCNode {
+pulic:
+    GObject();
+    virtual ~GObject(){
+    }
+    virtual void Load() {
+    }
+    virtual void SetPosition(float x, float y) {
+    }
+    virtual CCNode* Node() {
+        return NULL;
+    }
+    virtual void Step(float dt) {
+    }
+
+};
+
+
+#endif
+
